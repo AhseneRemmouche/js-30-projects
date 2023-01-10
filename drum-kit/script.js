56073fe0b0961ea70c9c8ecfb1a7611a7dedc00a
Ahsene Remmouche <ahsene.remmouche@outlook.com>
2023-01-10 15:26:31 -0500

    Addiing js code

--- drum-kit/script.js
+++ drum-kit/script.js
@@ -1,4 +1,22 @@
 console.log('Salam');
-window.addEventListener('keydown', function (e) {
-    console.log(e);
-});
+
+function playSound(e) {
+    var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    var key = this.document.querySelector(`div[data-key="${e.keyCode}"]`);
+    console.log(audio);
+    if (!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+console.log(keys);
+keys.forEach(key =>
+    key.addEventListener('transitionend', e => {
+        if (e.propertyName !== 'transform') return;
+        e.target.classList.remove('playing');
+    })
+);
+
+window.addEventListener('keydown', playSound);
